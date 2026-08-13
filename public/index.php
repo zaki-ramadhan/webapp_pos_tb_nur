d3294505f7d3df172219ca17079bf5eb907394aa
zaki-ramadhan <zakiram4dhan@gmail.com>
2026-08-13 13:03:29 +0700

    fix(hosting): make public/index.php support both local and cPanel directory structures

--- public/index.php
+++ public/index.php
@@ -6,15 +6,26 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+    require $maintenance;
+} elseif (file_exists($maintenance = __DIR__.'/../webapp_pos_tb_nur/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/vendor/autoload.php')) {
+    require __DIR__.'/vendor/autoload.php';
+} else {
+    require __DIR__.'/../webapp_pos_tb_nur/vendor/autoload.php';
+}
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+if (file_exists(__DIR__.'/bootstrap/app.php')) {
+    $app = require_once __DIR__.'/bootstrap/app.php';
+} else {
+    $app = require_once __DIR__.'/../webapp_pos_tb_nur/bootstrap/app.php';
+    $app->usePublicPath(__DIR__);
+}
 
 $app->handleRequest(Request::capture());
