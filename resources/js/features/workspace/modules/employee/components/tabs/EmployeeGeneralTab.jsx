3128e44f5cb9d868b3af4f5aa2a6b27d3744c8ce
zaki-ramadhan <zakiram4dhan@gmail.com>
2026-08-14 15:51:38 +0700

    Revert "refactor: simplify employee positions to Kasir and Kuli Muat / Kuli"
    This reverts commit 8c7929fec6a571fe93990a1316f34a57eef8c180.

--- resources/js/features/workspace/modules/employee/components/tabs/EmployeeGeneralTab.jsx
+++ resources/js/features/workspace/modules/employee/components/tabs/EmployeeGeneralTab.jsx
@@ -8,10 +8,9 @@ import { EmployeeFieldRow } from '@/features/workspace/modules/employee/employee
 import { SuggestionTextInput, ToggleSwitch } from '@/features/workspace/modules/employee/employeeControls';
 
 const POSITION_SUGGESTIONS = [
-    'Kasir',
-    'Kuli Muat',
-    'Kuli',
-    'Sopir',
+    'Kasir Toko',
+    'Staf Gudang & Muat',
+    'Sopir Pengiriman',
     'Pemilik Toko',
 ];
 
