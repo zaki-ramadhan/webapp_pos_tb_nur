3d678dc684352b87490a6a75e7636f16384ce423
zaki-ramadhan <zakiram4dhan@gmail.com>
2026-09-23 17:10:48 +0700

    fix(bootstrap): perbaiki deteksi path autoloader pada public/index.php untuk lingkungan lokal

--- public/index.php
+++ public/index.php
@@ -6,22 +6,28 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+} elseif (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 } elseif (file_exists($maintenance = __DIR__.'/../webapp_pos_tb_nur/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-if (file_exists(__DIR__.'/vendor/autoload.php')) {
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__.'/../vendor/autoload.php';
+} elseif (file_exists(__DIR__.'/vendor/autoload.php')) {
     require __DIR__.'/vendor/autoload.php';
-} else {
+} elseif (file_exists(__DIR__.'/../webapp_pos_tb_nur/vendor/autoload.php')) {
     require __DIR__.'/../webapp_pos_tb_nur/vendor/autoload.php';
 }
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-if (file_exists(__DIR__.'/bootstrap/app.php')) {
+if (file_exists(__DIR__.'/../bootstrap/app.php')) {
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+} elseif (file_exists(__DIR__.'/bootstrap/app.php')) {
     $app = require_once __DIR__.'/bootstrap/app.php';
 } else {
     $app = require_once __DIR__.'/../webapp_pos_tb_nur/bootstrap/app.php';
